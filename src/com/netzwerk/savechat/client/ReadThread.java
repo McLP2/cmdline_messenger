1c29aa33d3a7665a681b33e5aaa66893c3d007b6
McLP <wdv50jkc@gmail.com>
2018-12-02 08:43:03 +0100

    print session fingerprint

--- src/com/netzwerk/savechat/client/ReadThread.java
+++ src/com/netzwerk/savechat/client/ReadThread.java
@@ -31,6 +31,11 @@ public class ReadThread extends Thread {
                         break;
                     case 'k':
                         client.ptrkey = Crypt.publicKeyFromBytes(Crypt.decode(message.substring(1)));
+                        byte[] pubbytes = client.ptrkey.getEncoded();
+                        byte[] pubbytes2 = client.pubkey.getEncoded();
+                        byte[] hashbytes = new byte[pubbytes.length];
+                        for (int i = 0; i < pubbytes.length; i++) hashbytes[i] = (byte) (pubbytes[i] ^ pubbytes2[i]);
+                        System.out.println("Fingerprint: " + Crypt.hash(hashbytes));
                         break;
                     case 'e':
                         System.out.println(Crypt.decrypt(message.substring(1), client.prvkey));
