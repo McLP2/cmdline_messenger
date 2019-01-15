818030576359e8b31845eccc06714ed90cde7919
McLP <wdv50jkc@gmail.com>
2019-01-16 00:04:26 +0100

    cleanup

--- src/com/netzwerk/savechat/Crypt.java
+++ src/com/netzwerk/savechat/Crypt.java
@@ -7,6 +7,7 @@ import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
 import java.util.Base64;
 
 public class Crypt {
